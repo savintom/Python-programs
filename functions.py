50c0ab4981b6f6248d560b6e8a9e882e217f0672
savintom <47852201+savintom@users.noreply.github.com>
2019-03-05 15:03:51 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,27 @@
+def add(a,b):
+	c=a+b
+	return c
+def call():
+	d=add(5,7)
+	print(d)
+call()
+
+def three(a,b,c):
+	s=a+b+c
+	print(a,b,c,s)
+three(4,5,6)
+
+def savin(university='CMR'):
+	print(university)
+savin("IITA")
+savin("IITB")
+savin()
+
+def sav(a):
+	print(a)
+sav("hello, i am savin")
+
+def sayhi():
+	print("hi")
+sayhi()
+
